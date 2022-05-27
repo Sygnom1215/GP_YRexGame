dad62e79917f9c67a58f6f0882a977e7c0f44651
박소형 <77712386+Sygnom1215@users.noreply.github.com>
2022-05-27 11:33:53 +0900

    따로 만들어보고 구성 시작할 것

---
+++ Config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define MAZ_FRAME_RATE 400
+#define CONSOLE_EXIT_KEY 'q' // 게임 종료
+#define CONSOLE_JUMP_KEY ' ' // Space Bar
+#define TREE_SYMBOL 'T'		// Tree
+#define DINO_SYMBOL 'x'		// Dino
+#define BLANK_SYMBOL '-'
+#define INITAL_PADDING 2
+
+#define PLAY_AREA_SIZE  22
+#define MAX_TREE_DISTANCE  5
+#define FRAME_RATE_REDUCTION 50
+#define LEVEL_UP_FACTOR  3
+
+#define PLAYER_START_POSITION 0
+#define PLAYER_SPEED 1
+#define PLAYER_JUMP_SPAN 3
